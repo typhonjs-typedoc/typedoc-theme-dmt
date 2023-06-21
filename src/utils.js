abb79a62d7b4ecf39d8655063dce010b87aef81f
typhonrt <support@typhonjs.io>
2023-06-21 00:21:15 -0700

    add copyDirectory

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,28 @@
+import fs   from 'node:fs';
+import path from 'node:path';
+
+/**
+ * Copies a directory w/ `node:fs`.
+ *
+ * @param {string}   source - Source path.
+ *
+ * @param {string}   destination - Destination path.
+ */
+export function copyDirectory(source, destination)
+{
+   fs.mkdirSync(destination, { recursive: true });
+
+   fs.readdirSync(source, { withFileTypes: true }).forEach((entry) =>
+   {
+      let sourcePath = path.join(source, entry.name);
+      let destinationPath = path.join(destination, entry.name);
+
+      entry.isDirectory()
+       ? copyDirectory(sourcePath, destinationPath)
+       : fs.copyFileSync(sourcePath, destinationPath);
+   });
+}
+
 /**
  * Serializes value to a string escaping it so that it can be set as a web component attribute.
  *
