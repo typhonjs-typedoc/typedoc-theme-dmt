bac36b016735692f4d0f137b74dd952c13f06010
typhonrt <support@typhonjs.io>
2023-06-22 04:37:12 -0700

    Added key command to focus current navigation anchor.

--- src/components/events/keyCommands.js
+++ src/components/events/keyCommands.js
@@ -1,6 +1,7 @@
 /**
  * Provides the following global keyboard commands:
  * - <Alt-I>: Go to main index.
+ * - <Alt-N>: Scroll to current page in navigation panel and focus it.
  * - <Alt-O>: If available, focus first anchor in `On This Page` container.
  */
 export function keyCommands()
@@ -16,6 +17,32 @@ export function keyCommands()
             window.location.href = `${globalThis.dmtOptions.basePath}index.html`;
             event.preventDefault();
             break;
+
+         case 'KeyN':
+         {
+            const navEl = globalThis.document.querySelector('nav.tsd-navigation');
+            if (navEl)
+            {
+               // First attempt to focus the current anchor element.
+               const anchorEl = navEl.querySelector('a.current');
+               if (anchorEl)
+               {
+                  anchorEl.focus({ focusVisible: true });
+                  event.preventDefault();
+               }
+               else
+               {
+                  // Then attempt the focus first anchor.
+                  const firstAnchorEl = navEl.querySelector('a');
+                  if (firstAnchorEl)
+                  {
+                     firstAnchorEl.focus({ focusVisible: true });
+                     event.preventDefault();
+                  }
+               }
+            }
+            break;
+         }
 
          case 'KeyO':
          {
