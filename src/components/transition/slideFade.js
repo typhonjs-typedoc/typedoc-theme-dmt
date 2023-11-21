58ccb37babfa3ccaf8fe0856f39d2c245c3ba324
typhonrt <support@typhonjs.io>
2023-11-20 18:10:09 -0800

    Update slideFade to handle x axis

--- src/components/transition/slideFade.js
+++ src/components/transition/slideFade.js
@@ -1,5 +1,5 @@
-import { fade, slide }  from 'svelte/transition';
-import { linear }       from 'svelte/easing';
+import { fade, slide }  from '#svelte/transition';
+import { linear }       from '#svelte/easing';
 
 /**
  * Combines slide & fade transitions into a single transition. For options `easing` this is applied to both transitions,
@@ -7,32 +7,36 @@ import { linear }       from 'svelte/easing';
  *
  * @param {HTMLElement} node - The transition node.
  *
- * @param {object}      options - Optional parameters.
+ * @param {object}      [options] - Optional parameters.
+ *
+ * @param {'x' | 'y'}   [options.axis] - The sliding axis.
  *
  * @param {number}      [options.delay] - Delay in ms before start of transition.
  *
  * @param {number}      [options.duration] - Total transition length in ms.
  *
- * @param {Function}    [options.easing=linear] - The easing function to apply to both slide & fade transitions.
+ * @param {import('svelte/transition').EasingFunction}   [options.easing=linear] - The easing function to apply to both
+ *        slide & fade transitions.
  *
- * @param {Function}    [options.easingFade=linear] - The easing function to apply to the fade transition.
+ * @param {import('svelte/transition').EasingFunction}   [options.easingFade=linear] - The easing function to apply to
+ *        the fade transition.
  *
- * @param {Function}    [options.easingSlide=linear] - The easing function to apply to the slide transition.
+ * @param {import('svelte/transition').EasingFunction}   [options.easingSlide=linear] - The easing function to apply to
+ *        the slide transition.
  *
- * @returns {{duration: number, css: (function(*=): string), delay: number, easing: (x: number) => number}}
- *  Transition object.
+ * @returns {import('svelte/transition').TransitionConfig} Transition config.
  */
 export function slideFade(node, options)
 {
-   const fadeEasing = options.easingFade || options.easing || linear;
-   const slideEasing = options.easingSlide || options.easing || linear;
+   const fadeEasing = options.easingFade ?? options.easing ?? linear;
+   const slideEasing = options.easingSlide ?? options.easing ?? linear;
 
    const fadeTransition = fade(node);
-   const slideTransition = slide(node);
+   const slideTransition = slide(node, { axis: options.axis });
 
    return {
-      delay: options.delay || 0,
-      duration: options.duration || 500,
+      delay: options.delay ?? 0,
+      duration: options.duration ?? 500,
       easing: linear,
       css: (t) =>
       {
