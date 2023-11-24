cfbb049cc93ab9297758312c07f1d7c6b110e65c
typhonrt <support@typhonjs.io>
2023-11-23 16:46:05 -0800

    Adjust external

--- rollup.config.js
+++ rollup.config.js
@@ -43,7 +43,7 @@ export default [
 
    {
       input: 'src/components/index.js',
-      external: ['../main.js', './dmt-nav-web-component.js'],   // `main.js` is loaded from this bundle.
+      external: ['../main.js', './navigationIndex.js'],   // `main.js` is loaded from this bundle.
       output: {
          file: 'dist/assets/dmt-components.js',
          format: 'es',
