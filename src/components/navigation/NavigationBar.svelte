83febf9a6b4295d6b80f61b3415577ec98b7e417
typhonrt <support@typhonjs.io>
2023-12-01 23:38:27 -0800

    Provide separate open / close all buttons.

--- src/components/navigation/NavigationBar.svelte
+++ src/components/navigation/NavigationBar.svelte
@@ -1,35 +1,45 @@
 <script>
    import { getContext } from 'svelte';
-   import { derived }    from 'svelte/store';
 
    const { dmtSessionStorage } = /** @type {NavigationData} */ getContext('#navigationData');
 
-   const allSessionStores = [...dmtSessionStorage.stores()];
-
-   // Create a derived store from all session storage stores; on any update reduce all values and set `storeAllOpen`
-   // to whether all folders are opened or not. This controls the icon and title in the reactive statements below.
-   const storeAllOpen = derived(allSessionStores, (stores, set) =>
-    set(!!stores.reduce((previous, current) => previous & current, true)));
-
-   $: title = $storeAllOpen ? 'Close All' : 'Open All';
+   /**
+    * Close all session storage states to close all folders.
+    */
+   function onClickCloseAll()
+   {
+      for (const store of dmtSessionStorage.stores()) { store.set(false); }
+   }
 
    /**
-    * Reverses all session storage states to close or open all folders.
+    * Opens all session storage states to open all folders.
     */
-   function onClick()
+   function onClickOpenAll()
    {
-      const flippedState = !$storeAllOpen;
-      for (const store of dmtSessionStorage.stores()) { store.set(flippedState); }
+      for (const store of dmtSessionStorage.stores()) { store.set(true); }
    }
 </script>
 
 <section>
-   <button on:click={onClick}
-           on:pointerdown|stopPropagation
-           class:flipped-vertical={$storeAllOpen}
-           title={title}>
-      <svg viewBox="0 0 1024 1024">
+   <svg style="display: none;">
+      <symbol id=dmt-double-icon-arrow viewBox="0 0 1024 1024">
          <path d="M517.408 993.568l-0.448 0.256c-18.592-0.032-37.152-7.168-51.328-21.344L51.392 558.24c-27.904-27.904-28.32-74.624 0.224-103.2 28.768-28.768 74.784-28.672 103.2-0.224l362.272 362.272L879.36 454.816c27.904-27.904 74.624-28.32 103.2 0.224 28.768 28.768 28.672 74.784 0.224 103.2l-414.24 414.24c-13.92 13.92-32.512 20.992-51.2 21.056z m0-397.408l-0.448 0.256c-18.592-0.032-37.152-7.168-51.328-21.344l-414.24-414.24c-27.904-27.904-28.32-74.624 0.224-103.2 28.768-28.768 74.784-28.672 103.2-0.224L517.088 419.68 879.36 57.408c27.904-27.904 74.624-28.32 103.2 0.224 28.768 28.768 28.672 74.784 0.224 103.2l-414.24 414.24c-13.92 13.92-32.512 20.992-51.2 21.056z" />
+      </symbol>
+   </svg>
+
+   <button on:click={onClickOpenAll}
+           on:pointerdown|stopPropagation
+           title={'Open All'}>
+      <svg>
+         <use xlink:href="#dmt-double-icon-arrow"></use>
+      </svg>
+   </button>
+
+   <button on:click={onClickCloseAll}
+           on:pointerdown|stopPropagation
+           title={'Close All'}>
+      <svg class=flipped-vertical>
+         <use xlink:href="#dmt-double-icon-arrow"></use>
       </svg>
    </button>
 </section>
