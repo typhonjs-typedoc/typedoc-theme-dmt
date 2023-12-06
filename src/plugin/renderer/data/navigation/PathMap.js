9387a69a43da3a9d139809b7384a61949f5eec70
typhonrt <support@typhonjs.io>
2023-12-06 06:49:08 -0800

    Updated approach to parsing navigation index for module trees.

--- src/plugin/renderer/data/navigation/PathMap.js
+++ src/plugin/renderer/data/navigation/PathMap.js
@@ -1,38 +1,12 @@
 export class PathMap
 {
-   /**
-    * @type {PathMap}
-    */
-   #child;
-
-   /**
-    * Depth of path map.
-    *
-    * @type {number}
-    */
-   #depth;
-
-   /**
-    * Max depth of path maps.
-    *
-    * @type {number}
-    */
    #depthMax;
 
-   /**
-    * @type {PathMapData}
-    */
-   #map = new Map();
+   #rootMap = new MapNode();
 
-   /**
-    * @returns {PathMapData}
-    */
-   get map() { return this.#map; }
-
-   constructor(depthMax, depth = 0)
+   constructor(depthMax)
    {
       this.#depthMax = depthMax;
-      this.#depth = depth;
    }
 
    /**
@@ -40,80 +14,142 @@ export class PathMap
     */
    add(node)
    {
-      const firstSlashIndex = node.text.indexOf('/');
+      const pathSegments = node.text.split('/');
 
-      let rootPath = node.text;
-      let subPath;
-
-      if (firstSlashIndex >= 0)
-      {
-         rootPath = node.text.substring(0, firstSlashIndex);
-         subPath = node.text.substring(firstSlashIndex + 1);
-      }
-
-      if (!this.#map.has(rootPath)) { this.#map.set(rootPath, []); }
-
-      const pathArray = this.#map.get(rootPath);
-
-      if (subPath)
-      {
-         const rootPathRegex = new RegExp(`^${rootPath}`);
-         node.text = node.text.replace(rootPathRegex, '');
-      }
-
-      pathArray.push({
-         subPath,
-         node
-      });
+      this.#addToMap(this.#rootMap, pathSegments, node, 0);
    }
 
-   #createChildMap()
+   #addToMap(currentMap, pathSegments, node, currentDepth)
    {
-      if (this.#child) { return this.#child; }
-
-      if (this.#depth < this.#depthMax - 1)
+      if (pathSegments.length === 0)
       {
-         this.#child = new PathMap(this.#depthMax, this.#depth + 1);
+         node.text = '';
+         currentMap.node = node;
+
+         return;
       }
 
-      return this.#child;
+      const currentSegment = pathSegments.shift();
+
+      if (!currentMap.map.get(currentSegment))
+      {
+         currentMap.map.set(currentSegment, new MapNode());
+      }
+
+      this.#addToMap(currentMap.map.get(currentSegment), pathSegments, node, currentDepth + 1);
    }
 
-   *process()
+   /**
+    * Constructs the output tree from the MapNode structure, considering depthMax.
+    *
+    * @returns {import('typedoc').NavigationElement[]} - The root of the reconstituted tree structure.
+    */
+   buildTree()
    {
-      for (const rootPath of this.#map.keys())
+      return this.#buildTree(this.#rootMap, 0, void 0).children;
+   }
+
+   #buildTree(currentMapNode, currentDepth, pathPrefix, depthMaxParent = undefined)
+   {
+      if (!currentMapNode) { return; }
+
+      let node;
+
+      if (currentMapNode.node)
       {
-         const entries = this.#map.get(rootPath);
-
-         let targetNode = {
-            text: rootPath,
-            children: []
-         };
-
-         const indexNoSubPath = entries.findIndex((entry) => entry.subPath === void 0);
-         if (indexNoSubPath >= 0)
+         // Create a new node with a potentially updated text field.
+         node = currentMapNode.node;
+         if (pathPrefix)
          {
-            targetNode = entries[indexNoSubPath].node;
-            entries.splice(indexNoSubPath, 1);
-
-            if (entries.length && !Array.isArray(targetNode.children)) { targetNode.children = []; }
+            // node.text = `${pathPrefix}/${node.text}`;
+            node.text = pathPrefix;
          }
-
-         for (const entry of entries)
-         {
-            const node = entry.node;
-            targetNode.children.unshift(node);
-         }
-
-         yield targetNode;
       }
+      else
+      {
+         node = { text: pathPrefix, children: [] };
+      }
+
+      if (!depthMaxParent && currentDepth === this.#depthMax)
+      {
+         // Save the current node as the depthMaxParent for siblings.
+         depthMaxParent = node;
+      }
+
+      // If at or beyond max depth, concatenate remaining path segments.
+      if (currentDepth >= this.#depthMax)
+      {
+         for (const [key, mapNode] of currentMapNode.map.entries())
+         {
+            const newPrefix = pathPrefix ? `${pathPrefix}/${key}` : key;
+            const childNode = this.#buildTree(mapNode, currentDepth + 1, newPrefix, depthMaxParent);
+            if (childNode)
+            {
+               if (depthMaxParent)
+               {
+                  // Add childNode as a sibling to the depthMaxParent.
+                  depthMaxParent.children.unshift(childNode);
+               }
+               else
+               {
+                  node.children.unshift(childNode);
+               }
+            }
+         }
+      }
+      else
+      {
+         for (const [key, mapNode] of currentMapNode.map.entries())
+         {
+            const childNode = this.#buildTree(mapNode, currentDepth + 1, key, depthMaxParent);
+            if (childNode)
+            {
+               if (!Array.isArray(node.children)) { node.children = []; }
+
+               node.children.unshift(childNode);
+            }
+         }
+      }
+
+      return node;
+   }
+
+   /**
+    * @returns {MapNode}
+    */
+   get root()
+   {
+      return this.#rootMap;
    }
 }
 
-/**
- * @typedef {{ subPath: string, node: import('typedoc').NavigationElement }} PathMapNode
- */
+class MapNode
+{
+   map = new Map();
 
-/**
- * @typedef {Map<string, PathMapNode[]>} PathMapData
- */
+   node = void 0;
+
+   /**
+    * Converts the MapNode structure to a nested object for easy logging.
+    *
+    * @returns {object}
+    */
+   toObject()
+   {
+      const obj = {};
+
+      for (const [key, value] of this.map.entries())
+      {
+         obj[key] = value instanceof MapNode ? value.toObject() : value;
+      }
+
+      if (this.node !== void 0)
+      {
+         obj.___node = {
+            text: this.node.text,
+            childrenLength: this.node?.children?.length
+         };
+      }
+      return obj;
+   }
+}
