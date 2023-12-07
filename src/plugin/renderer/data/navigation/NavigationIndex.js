5f84394f11a7d249972b449d2162633fbdbe377c
typhonrt <support@typhonjs.io>
2023-12-07 04:15:05 -0800

    Support navigation index transformation when Typedoc option `navigation: { includeGroups: true }` is enabled.

--- src/plugin/renderer/data/navigation/NavigationIndex.js
+++ src/plugin/renderer/data/navigation/NavigationIndex.js
@@ -19,34 +19,74 @@ export class NavigationIndex
       // No processing necessary so directly return the index.
       if (options.navModuleDepth === 0) { return index; }
 
-      return this.parseModuleTree(index, options.navModuleDepth);
+      return this.parseModuleTree(index, options);
    }
 
    /**
     * @param {import('typedoc').NavigationElement[]}  index - Navigation index.
     *
-    * @param {number} depthMax - Max depth to parse.
+    * @param {ThemeOptions} options - Theme options.
     *
     * @returns {import('typedoc').NavigationElement[]} Processed navigation index.
     */
-   static parseModuleTree(index, depthMax)
+   static parseModuleTree(index, options)
    {
-      const moduleTreeMap = new ModuleTreeMap(depthMax);
+      const moduleTreeMap = new ModuleTreeMap(options.navModuleDepth);
 
+      let moduleGroup;
+
+      // Determine if there is a top level "Modules" group node. This is the case when Typedoc option
+      // `navigation: { includeGroups: true }` is set.
       for (let i = index.length; --i >= 0;)
       {
          const node = index[i];
-
-         // For all module NavigationElements add to the module tree map and remove original from navigation index.
-         if (node.kind === ReflectionKind.Module)
+         if (node?.text === 'Modules' && Array.isArray(node.children) &&
+          node.children?.[0]?.kind === ReflectionKind.Module)
          {
-            moduleTreeMap.add(node);
+            moduleGroup = node;
 
-            index.splice(i, 1);
+            // Potentially change text to `Packages` given `moduleAsPackage` option.
+            if (options.moduleAsPackage) { moduleGroup.text = 'Packages'; }
+
+            break;
          }
       }
 
-      index.unshift(...moduleTreeMap.buildTree());
+      // Handle the case when modules are in the module group node.
+      if (moduleGroup)
+      {
+         for (let i = moduleGroup.children.length; --i >= 0;)
+         {
+            const node = moduleGroup.children[i];
+
+            // For all module NavigationElements add to the module tree map and remove original from navigation index.
+            if (node.kind === ReflectionKind.Module)
+            {
+               moduleTreeMap.add(node);
+
+               moduleGroup.children.splice(i, 1);
+            }
+         }
+
+         moduleGroup.children.unshift(...moduleTreeMap.buildTree());
+      }
+      else // Filter the main index.
+      {
+         for (let i = index.length; --i >= 0;)
+         {
+            const node = index[i];
+
+            // For all module NavigationElements add to the module tree map and remove original from navigation index.
+            if (node.kind === ReflectionKind.Module)
+            {
+               moduleTreeMap.add(node);
+
+               index.splice(i, 1);
+            }
+         }
+
+         index.unshift(...moduleTreeMap.buildTree());
+      }
 
       return index;
    }
