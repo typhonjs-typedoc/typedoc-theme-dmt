93debe30a3a53dd6741b394ce99bfd6e737c2b7f
typhonrt <support@typhonjs.io>
2024-09-06 02:28:39 -0700

    Update types.

--- src/frontend/state/types.d.ts
+++ src/frontend/state/types.d.ts
@@ -1,6 +1,9 @@
 import type { NavigationElement }   from 'typedoc';
 
-export type DMTNavigationElement = NavigationElement & {
+/**
+ * Augments the TypeDoc NavigationElement with additional state data.
+ */
+type DMTNavigationElement = NavigationElement & {
    /**
     * A storage key if this element / entry is a tree node w/ children.
     */
@@ -11,4 +14,39 @@ export type DMTNavigationElement = NavigationElement & {
     * tree nodes where the path URL matches an entry path.
     */
    opened?: boolean;
+}
+
+/**
+ * Defines an icon in {@link DMTToolbarIconLinks}.
+ */
+type DMTToolbarIconLink = {
+   /**
+    * URL for icon.
+    */
+   iconURL?: string;
+
+   /**
+    * Element title for hover / display.
+    */
+   title?: string;
+
+   /**
+    * Destination URL.
+    */
+   url: string;
+}
+
+/**
+ * Defines the data for the `toolbarIconLinks` store for `IconLinks.svelte`.
+ */
+type DMTToolbarIconLinks = {
+   icons: Iterable<DMTToolbarIconLink>;
+
+   totalWidth?: number;
+}
+
+export {
+   DMTNavigationElement,
+   DMTToolbarIconLink,
+   DMTToolbarIconLinks
 }
