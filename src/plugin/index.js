749d99c26d49a98a6188b6dd812ee1544392e854
typhonrt <support@typhonjs.io>
2025-10-21 01:39:36 -0700

    Fix DMT bootstrapping for mono-repos.

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -41,68 +41,88 @@ export function load(app)
          // Adjust default theme options. Must do so here at bootstrap end before options are locked.
          ThemeOptions.adjustDefaultOptions(app);
 
-         // Initialize the PageRenderer in the Converter event begin to parse options before conversion.
+         let bootstrapped = false;
+
+         // Bootstrap at Converter event begin to parse options before conversion. TypeDoc doesn't provide a clean
+         // way to access the completed app `options` loading until this point and doesn't offer another event callback.
+         //
+         // Mono-repos / multiple project conversion will invoke `Converter.EVENT_BEGIN` multiple times so just
+         // bootstrap once.
          app.converter.on(Converter.EVENT_BEGIN, () =>
          {
-            const options = new ThemeOptions(app);
-
-            // Make the `dmt` sub-folder on `assets`.
-            app.renderer.on(RendererEvent.BEGIN, (event) => fs.mkdirSync(path.join(event.outputDirectory, 'assets',
-             'dmt'), { recursive: true }));
-
-            // Transform / parse TypeDoc navigation index.
-            app.renderer.on(RendererEvent.BEGIN, (event) => NavigationIndex.transform(app, event.project, options));
-
-            // At the end of rendering generate the compressed global component data.
-            app.renderer.on(RendererEvent.END, (event) => GlobalResources.build(event, app, options), -100);
-
-            // TODO: REMOVE TYPEDOC 0.26 CHANGES - `removeComponent` no longer available.
-            // ------------------------------------------
-            // Remove unused default theme components.
-            // app.renderer.removeComponent('javascript-index');
-            // app.renderer.removeComponent('navigation-tree');
-            // ------------------------------------------
-
-            // Add DMT components.
-            new PageRenderer(app, options);
-
-            // Selectively load search index creation.
-            if (options.search) { new SearchIndexPackr(app, options); }
-
-            // TODO: Finish implementing `searchQuick` functionality.
-            // if (options.searchQuick) { new SearchQuickIndexPackr(app); }
-
-            // Remove unused JSDoc comment block tags from classes; for ESM.
-            app.converter.on(Converter.EVENT_RESOLVE, (context, reflection) =>
+            if (!bootstrapped)
             {
-               if (reflection?.kind === ReflectionKind.Class)
-               {
-                  if (Array.isArray(reflection?.comment?.blockTags))
-                  {
-                     const blockTags = reflection.comment.blockTags;
-                     for (let cntr = blockTags.length; --cntr >= 0;)
-                     {
-                        if (jsdocClassTagsRemove.has(blockTags[cntr]?.tag)) { blockTags.splice(cntr, 1); }
-                     }
-                  }
-               }
-            });
-
-            // Handle any module name substitution during project conversion.
-            if (Object.keys(options.moduleRemap.names).length)
-            {
-               app.converter.on(Converter.EVENT_RESOLVE, (context, reflection) =>
-               {
-                  // Remap module to package names.
-                  if (reflection?.kind === ReflectionKind.Module && reflection.name in options.moduleRemap.names)
-                  {
-                     reflection.name = options.moduleRemap.names[reflection.name];
-                  }
-               });
+               bootstrap(app);
+               bootstrapped = true;
             }
          });
       }
    });
 
    app.renderer.defineTheme('default-modern', DefaultModernTheme);
+}
+
+/**
+ * Bootstraps the DMT lifecycle.
+ *
+ * @param {import('typedoc').Application} app - Typedoc Application
+ */
+function bootstrap(app)
+{
+   const options = new ThemeOptions(app);
+
+   // Make the `dmt` sub-folder on `assets`.
+   app.renderer.on(RendererEvent.BEGIN, (event) => fs.mkdirSync(path.join(event.outputDirectory, 'assets',
+    'dmt'), { recursive: true }));
+
+   // Transform / parse TypeDoc navigation index.
+   app.renderer.on(RendererEvent.BEGIN, (event) => NavigationIndex.transform(app, event.project, options));
+
+   // At the end of rendering generate the compressed global component data.
+   app.renderer.on(RendererEvent.END, (event) => GlobalResources.build(event, app, options), -100);
+
+   // TODO: REMOVE TYPEDOC 0.26 CHANGES - `removeComponent` no longer available.
+   // ------------------------------------------
+   // Remove unused default theme components.
+   // app.renderer.removeComponent('javascript-index');
+   // app.renderer.removeComponent('navigation-tree');
+   // ------------------------------------------
+
+   // Add DMT components.
+   new PageRenderer(app, options);
+
+   // Selectively load search index creation.
+   if (options.search) { new SearchIndexPackr(app, options); }
+
+   // TODO: Finish implementing `searchQuick` functionality.
+   // if (options.searchQuick) { new SearchQuickIndexPackr(app); }
+
+   // Remove unused JSDoc comment block tags from classes; for ESM.
+   app.converter.on(Converter.EVENT_RESOLVE, (context, reflection) =>
+   {
+      if (reflection?.kind === ReflectionKind.Class)
+      {
+         if (Array.isArray(reflection?.comment?.blockTags))
+         {
+            const blockTags = reflection.comment.blockTags;
+            for (let cntr = blockTags.length; --cntr >= 0;)
+            {
+               if (jsdocClassTagsRemove.has(blockTags[cntr]?.tag)) { blockTags.splice(cntr, 1); }
+            }
+         }
+      }
+   });
+
+   // Handle any module name substitution during project conversion.
+   if (Object.keys(options.moduleRemap.names).length)
+   {
+      app.converter.on(Converter.EVENT_RESOLVE, (context, reflection) =>
+      {
+         // Remap module to package names.
+         if (reflection?.kind === ReflectionKind.Module && reflection.name in options.moduleRemap.names)
+         {
+            reflection.name = options.moduleRemap.names[reflection.name];
+         }
+      });
+   }
 }
